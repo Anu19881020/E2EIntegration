b3504635c22346f3d8f2b6c7eae8f0c7b937235f
Anuradha <Anuradha@10.0.0.90>
2020-05-17 23:13:24 -0400

    Created Automation Project

---
+++ ETE_JenkinsSetup/src/jenkin/TC_003.java
@@ -0,0 +1,19 @@
+package jenkin;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.annotations.Test;
+
+public class TC_003 {
+
+	@Test
+	public void TS_001() {
+	System.setProperty("webdriver.gecko.driver", "D:\\Java\\ETE_JenkinsSetup\\IEDriverServer");
+	InternetExplorerDriver driver= new InternetExplorerDriver();
+	driver.get("https://www.facebook.com");
+	driver.findElement(By.id("email")).sendKeys("hello");
+	driver.findElement(By.id("pass")).sendKeys("hello");
+	driver.quit();
+	}
+}
